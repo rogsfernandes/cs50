6aca1c8f071fe53f105e210785ad2cdd090f4193
Roger Santos <rsfernandes@outlook.com>
2021-03-28 21:32:28 -0300

    Adds mario exercise in Python

---
+++ week-6/mario.py
@@ -0,0 +1,27 @@
+from cs50 import get_int
+
+
+def main():
+    height = get_int("Height: ")
+    while height < 1 or height > 8:
+        height = get_int("Height: ")
+    print_pyramid(height)
+
+
+def print_pyramid(height, original_height=None):
+    if original_height == None:
+        original_height = height
+    if height == 0:
+        return
+
+    print_pyramid(height-1, original_height)
+    blocks = ""
+
+    print("".rjust(original_height - height), end="")
+    for i in range(height):
+        blocks += "#"
+    print(f"{blocks}  {blocks}")
+
+
+if __name__ == "__main__":
+    main()
